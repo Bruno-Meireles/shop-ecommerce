43be9f79eae08057e383707dcc9a87adbe9934a3
Bruno Meireles <bruno.meirelessilva90@gmail.com>
2024-08-17 00:24:05 -0300

    Adding Function menu hamburguer

--- js/header.js
+++ js/header.js
@@ -1,20 +1,25 @@
+// Create variables
+
 const menuIcon = document.querySelector(".menu");
 const headerNav = document.querySelector(".header-nav");
 const headerTitle = document.querySelector(".header-title");
 const theeIconsHeader = document.querySelector(".icons-car-person");
-
-menuIcon.addEventListener("click", () => {
-    headerNav.classList.toggle("show");
-    theeIconsHeader.classList.toggle("hide");
-  if (headerNav.classList.contains("show")) {
-    headerTitle.style.display = "none";
-  }else{
-    headerTitle.style.display = "block";
-  }
-});
-
 const closeBtn = document.querySelector(".close-btn");
 
+// function  show menu
+menuIcon.addEventListener("click", () => {
+  headerNav.classList.add("show");
+  theeIconsHeader.classList.add("hide");
+  menuIcon.style.display = "none"; // Hide the menu icon
+  closeBtn.classList.add("show");
+  headerTitle.style.display = "none";
+});
+
+// function close menu
 closeBtn.addEventListener("click", () => {
-  menuIcon.classList.remove("show");
+  headerNav.classList.remove("show");
+  theeIconsHeader.classList.remove("hide");
+  menuIcon.style.display = "block"; // Show the menu icon
+  closeBtn.classList.remove("show");
+  headerTitle.style.display = "block";
 });
