072da25199c58d1309f316de1b5bbb6094392a9c
Bruno Meireles <bruno.meirelessilva90@gmail.com>
2024-08-19 10:31:27 -0300

    Adding custmer card in js

--- js/customers.js
+++ js/customers.js
@@ -1,51 +1,96 @@
-function createCusroemerCard(customerData) {
-  const customerAll = document.createElement("div");
-  customerAll.classList.add("customer-all");
-
-  const customersCard = document.createElement("div");
-  customersCard.classList.add("customers-card");
+function createCustomerCard(customerData) {
+  const customerCard = document.createElement("div");
+  customerCard.classList.add("customers-card");
 
   const image = document.createElement("img");
   image.src = customerData.imageUrl;
-  image.alt = "customer image";
-  customersCard.appendChild(image);
+  image.alt = "Customer Image";
+  image.classList.add("customer-image");
+  customerCard.appendChild(image);
+
+  const customer = document.createElement("div");
+  customer.classList.add("customer-content");
 
   const customerName = document.createElement("h2");
-  customerName.innerText = customerData.title;
-  customersCard.appendChild(customerName);
+  customerName.classList.add("customer-name");
+  customerName.textContent = customerData.title;
+    customer.appendChild(customerName);
 
-  const customerP = document.createElement("p");
-  customerP.classList.add("customer-p");
-  customerP.textContent = customerData.paragraph;
-  customersCard.appendChild(customerP);
+  const okIcon = document.createElement("img");
+  okIcon.src = "src/assets/icons/ok.svg";
+  okIcon.alt = "Ok Icon";
+  okIcon.classList.add("ok-icon");
+  customerName.appendChild(okIcon);
+
+  const customerQuote = document.createElement("p");
+  customerQuote.classList.add("customer-quote");
+  customerQuote.textContent = customerData.paragraph;
+  customer.appendChild(customerQuote);
+
+
+
+  customerCard.appendChild(customer);
+  // customer.appendChild(customer);
+
+  return customerCard;
 }
 
-const customersData1 = {
-  imageUrl: "src/assets/icons/five-star.svg",
-  title: "Sarah M.",
-  paragraph:
-    "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.” ",
-};
-const customersData2 = {
-  imageUrl: "src/assets/icons/five-star.svg",
-  title: "Sarah M.",
-  paragraph:
-    "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.” ",
-};
+const customersData = [
+  {
+    imageUrl: "src/assets/icons/five-star.svg",
+    title: "Sarah M. ",
+    paragraph:
+      "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.",
+  },
 
-const container = document.querySelector(".customers-all");
-container.appendChild(customersData1);
+  {
+    imageUrl: "src/assets/icons/five-star.svg",
+    title: "Alex K.",
+    paragraph:
+      "The service at Shop.co is outstanding. Fast delivery, high-quality products, and they always exceed my expectations.",
+  },
+  {
+    imageUrl: "src/assets/icons/five-star.svg",
+    title: "James L.",
+    paragraph:
+      "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.”",
+  },
+  {
+    imageUrl: "src/assets/icons/five-star.svg",
+    title: "Mooen",
+    paragraph:
+      "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.”",
+  },
+  {
+    imageUrl: "src/assets/icons/five-star.svg",
+    title: "Leblon James",
+    paragraph:
+      "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.”",
+  },
+  {
+    imageUrl: "src/assets/icons/five-star.svg",
+    title: "Rita",
+    paragraph:
+      "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.”",
+  },
+];
 
-const customerAll = createCusroemerCard(customersData1);
-const customerAll2 = createCusroemerCard(customersData2);
+const customersContainer = document.getElementById("customers-all");
 
+customersData.forEach((data) => {
+  const customerCard = createCustomerCard(data);
+  customersContainer.appendChild(customerCard);
+});
+
+// function dor arrow
 const arrowLeft = document.querySelector("#arrow-left");
 const arrowRight = document.querySelector("#arrow-rigth");
 const customersAll = document.querySelector(".customers-all");
 
 arrowLeft.addEventListener("click", () => {
-  customersAll.scrollLeft -= 600;
+  customersAll.scrollLeft -= 390;
 });
+
 arrowRight.addEventListener("click", () => {
-  customersAll.scrollLeft += 600;
+  customersAll.scrollLeft += 390;
 });
